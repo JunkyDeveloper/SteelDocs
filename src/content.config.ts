94ebe74b65c9b756e7058de4ba3e0ffd36c0497f
tn-lorenz <contact@tn-lorenz.de>
2026-01-25 00:57:31 +0100

    feat: replace occurences of version tag with static variable (maybe I have overlooked some)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 };
+
+export const VERSION = "1.21.11";
